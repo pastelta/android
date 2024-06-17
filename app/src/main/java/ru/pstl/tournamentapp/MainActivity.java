8e0d72ebb8d403bd2a126d696894350b91bc9726
Tatiana Pastel <pastel.t@yandex.ru>
2024-06-17 23:23:20 +0400

    corrected AndroidManifest.xml

--- app/src/main/java/ru/pstl/tournamentapp/MainActivity.java
+++ app/src/main/java/ru/pstl/tournamentapp/MainActivity.java
@@ -1,20 +1,15 @@
 package ru.pstl.tournamentapp;
 
+import android.view.Gravity;
 import android.view.View;
 import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private LinearLayout layoutList;
-    private Button buttonAdd;
-    private Button buttonUpdate;
-    private Integer counter = 1;
+    LinearLayout layoutList;
     private EditText buying_in;
+    int counter = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,24 +17,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         layoutList = findViewById(R.id.layout_list);
-        buttonAdd = findViewById(R.id.button_add);
-        buying_in = (EditText) findViewById(R.id.buying_in);
-        buttonUpdate = findViewById(R.id.button_submit_list);
+        Button buttonAdd = findViewById(R.id.button_add);
+        buying_in = findViewById(R.id.buying_in);
+        Button buttonUpdate = findViewById(R.id.button_submit_list);
         buttonAdd.setOnClickListener(this);
 
         buttonUpdate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Toast.makeText(MainActivity.this, "getChildCount " + layoutList.getChildCount(), Toast.LENGTH_SHORT).show();
-                if(buying_in.getText().length() == 0) buying_in.setText(String.valueOf(0));
-
-                for(int i = 0; i<layoutList.getChildCount(); i++){
+                if (buying_in.getText().length() == 0) buying_in.setText(String.valueOf(0));
+                int amount = 0;
+                for (int i = 0; i < layoutList.getChildCount(); i++) {
                     TextView e = layoutList.getChildAt(i).findViewById(R.id.entry);
-                    EditText b = ((EditText) findViewById(R.id.buying_in));
+                    EditText b = findViewById(R.id.buying_in);
                     Integer pay_in_res = Integer.parseInt(e.getText().toString()) * Integer.parseInt(b.getText().toString());
                     TextView p_in = layoutList.getChildAt(i).findViewById(R.id.pay_in);
                     p_in.setText(String.valueOf(pay_in_res));
+                    amount += pay_in_res;
                 }
+                Toast toast = Toast.makeText(getApplicationContext(), "Amount: " + amount, Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP|Gravity.END, 0, 0);
+                toast.show();
             }
         });
     }
@@ -52,12 +51,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
     private void addView(){
             final View PLAYER_VIEW = getLayoutInflater().inflate(R.layout.row_add_player, null, false);
-            EditText editText = (EditText) PLAYER_VIEW.findViewById(R.id.edit_player_name);
-            ImageView imageClose = (ImageView) PLAYER_VIEW.findViewById(R.id.image_remove);
 
-            ImageView imageEntryMin = (ImageView) PLAYER_VIEW.findViewById(R.id.image_entry_min);
+            ImageView imageClose = PLAYER_VIEW.findViewById(R.id.image_remove);
+
+            ImageView imageEntryMin = PLAYER_VIEW.findViewById(R.id.image_entry_min);
             TextView entry = PLAYER_VIEW.findViewById(R.id.entry);
-            ImageView imageEntryMax = (ImageView) PLAYER_VIEW.findViewById(R.id.image_entry_max);
+            ImageView imageEntryMax = PLAYER_VIEW.findViewById(R.id.image_entry_max);
             TextView pay_in = PLAYER_VIEW.findViewById(R.id.pay_in);
             pay_in.setText(String.valueOf(Integer.parseInt(buying_in.getText().toString())));
 
